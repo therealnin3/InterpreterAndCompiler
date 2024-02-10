e0bbd4af4492b2abd8ff4af81d9f9e57504b5be2
GJ Viviers <hardusviviers@gmail.com>
2024-02-10 23:00:22 +0100

    part 1

---
+++ Compiler/teenytiny.py
@@ -0,0 +1,14 @@
+from lex import *
+
+
+def main():
+    source = "IF+-123 foo*THEN/"
+    lexer = Lexer(source)
+
+    token = lexer.getToken()
+    while token.kind != TokenType.EOF:
+        print(token.kind)
+        token = lexer.getToken()
+
+
+main()
